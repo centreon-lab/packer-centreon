bc4ecf5aedfbfb22ebcee16ac183a36ba5feac27
Luiz G. Costa <luizgustavo@luizgustavo.pro.br>
2019-08-14 16:40:34 +0100

    fix cleanup  script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,11 +3,15 @@
 yum -y clean all
 
 # Clean network configuration
-sed -i '/HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth0
-sed -i "/^UUID/d" /etc/sysconfig/network-scripts/ifcfg-eth0
-sed -i "/^UUID/d" /etc/sysconfig/network-scripts/ifcfg-eth0
+ip -o link show | awk -F': ' '{print $2}' | grep -v lo | while read INTERFACE; do
+    if [ -e /etc/sysconfig/network-scripts/ifcfg-${INTERFACE} ]; then
+        sed -i '/HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-${INTERFACE}
+        sed -i "/^UUID/d" /etc/sysconfig/network-scripts/ifcfg-${INTERFACE}
+        sed -i "/^UUID/d" /etc/sysconfig/network-scripts/ifcfg-${INTERFACE}
+    fi
+    rm -f /var/lib/dhclient/dhclient-${INTERFACE}.leases
+done
 rm -f /etc/ssh/ssh_host_*
 rm -f /etc/udev/rules.d/70-persistent-net.rules
-rm -f /var/lib/dhclient/dhclient-eth0.leases
 
 rm -rf /tmp/*
