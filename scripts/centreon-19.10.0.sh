cb370373fb43edd4de8e29564467b8ce4fce724f
Luiz G. Costa <luizgustavo@luizgustavo.pro.br>
2019-08-14 12:31:29 +0100

    set password mysql, text install and mem,cpu

--- scripts/centreon-19.10.0.sh
+++ scripts/centreon-19.10.0.sh
@@ -248,7 +248,8 @@ EOF
 yum install -y centreon
 
 echo "date.timezone = Europe/Paris" > /etc/opt/rh/rh-php72/php.d/php-timezone.ini
-systemctl start mysql
+mysqladmin -u root password $MYSQL_ROOT_PASSWORD # Set password to root mysql
+systemctl restart mysql
 systemctl restart rh-php72-php-fpm
 systemctl restart httpd24-httpd
 sleep 5 # waiting start httpd process
